ea104e674dac047e0c883a6d8739374c8ec02130
pbl.gllgs <pbl.gllgs@gmail.com>
2023-04-14 13:01:16 -0400

    config server

--- src/main/java/com/pblgllgs/loans/controller/LoansController.java
+++ src/main/java/com/pblgllgs/loans/controller/LoansController.java
@@ -1,9 +1,15 @@
 package com.pblgllgs.loans.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.pblgllgs.loans.config.LoansServiceConfig;
 import com.pblgllgs.loans.model.Customer;
 import com.pblgllgs.loans.model.Loans;
+import com.pblgllgs.loans.model.Properties;
 import com.pblgllgs.loans.repository.LoansRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,9 @@ public class LoansController {
     @Autowired
     private LoansRepository loansRepository;
 
+    @Autowired
+    LoansServiceConfig loansConfig;
+
     @PostMapping("/myLoans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer){
         List<Loans> loans
@@ -25,5 +34,14 @@ public class LoansController {
         }else {
             return null;
         }
+    }
+
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+                loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
